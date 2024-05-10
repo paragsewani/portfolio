304845b5744a5c1cd0018a48f69af6f451c51aec
paragsewani <142333190+paragsewani@users.noreply.github.com>
2024-05-11 00:45:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+var headingload=document.getElementsByClassName(".page1 h1");
+function loadinganimation(headingload){
+ for(var i=0;i<1;i++){
+  var headelement=document.getElementsByClassName(".page1 .heading");
+ opacity:0;
+ duration:0.2;
+ delay:0.2;
+
+ } 
+  
+
+}
+loadinganimation();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+  
+ 
+
